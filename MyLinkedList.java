d0fa264af75969297dcd68e5f1400e152b945844
akimaliiev <103598323+akimaliiev@users.noreply.github.com>
2022-04-15 12:24:34 +0600

    Add files via upload

---
+++ MyLinkedList.java
@@ -0,0 +1,47 @@
+public class MyLinkedList<T> {
+    private static class MyNode<T>{
+        T data;
+        MyNode<T> next, prev;
+
+        MyNode(T data) {
+            this.data = data;
+        }
+    }
+
+    private int length = 0;
+    private MyNode<T> head, tail;
+
+    public MyLinkedList() {}
+
+    public void add(T item) {
+        MyNode<T> newNode = new MyNode<>(item);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            newNode.prev = tail;
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    public T get(int index) {
+        if (index >= length || index < 0)
+            throw new IndexOutOfBoundsException("index should be positive and less than size");
+
+        MyNode<T> temp = head;
+
+        while (index != 0) {
+            temp = temp.next;
+            index--;
+        }
+
+        return temp.data;
+    }
+
+    public int size() {
+        return length;
+    }
+
+}
+
